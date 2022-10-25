83aaa7fe8279fc377782be655e30a1b404b537f5
winn-tech <olugodwin99@gmail.com>
2022-10-25 09:00:09 +0100

    update

--- script.js
+++ script.js
@@ -2,26 +2,37 @@ window.addEventListener("load",  ()=>{
 
     let submit = document.getElementById("submit");
     var list = document.getElementById("text");
-    let ul = document.querySelector("ul");
+    let listValue= list.value;
+    let div = document.getElementById("parent");
     let form = document.querySelector(".form")
     let spans;
-
+    
 form.addEventListener("submit",  (e)=> {
     e.preventDefault();
-    var listItem =document.createElement("li");
+    var listItem =document.createElement("p");
     var span = document.createElement("span")
-    var cancel = document.createTextNode("\u00d7")
+    var secondSpan = document.createElement("span")
+    var thirdSpan = document.createElement("span")
+    // var cancel = document.createTextNode("\u00d7")
+    var cancel = document.createTextNode("remove \u00d7")
+    const d = new Date();
+    let text = d.toString();
+    let play = text.slice(0,  24);
+    secondSpan.append(play);
     span.appendChild(cancel);
-    console.log(span);
-    console.log(cancel)
-    listItem.append(list.value)
+    let listValue= list.value;
+    thirdSpan.append(listValue)
+    listItem.append(thirdSpan)
     listItem.appendChild(span)
+    listItem.append(secondSpan)
     form.reset();
-    ul.append(listItem);
+    div.append(listItem);
     spans = span;
+    spans.setAttribute("id", "cancel")
+    secondSpan.setAttribute("id", "date");
+    thirdSpan.setAttribute("id", "event");
     spans.addEventListener("click", (e)=> {
         e.target.parentElement.style.display= "none";
-        
     })
 })
 
@@ -29,9 +40,5 @@ form.addEventListener("submit",  (e)=> {
 
 
     
-
-
-
-
 
 
